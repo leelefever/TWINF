9373f2e65e353eba5cbe00a9759984924aa923d3
Jared Stoneberg <jared@number10webcompany.com>
2017-04-05 13:44:27 -0700

    Initial commit of all TWINF flat site files.

---
+++ files/includes/sites-theworldisnotflat.com-modules-maps-scripts-controls.js
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60446936-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<body>
+<h1>Not Found</h1>
+<p>The requested URL /theworldisnotflat.com/modules/maps/scripts/controls.js was not found on this server.</p>
+<hr>
+<address>Apache/2.2.15 (CentOS) Server at web.archive.orgsites Port 80</address>
+</body></html>
